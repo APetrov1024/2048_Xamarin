a535c8bef827ac79083141bf9430035199fa2937
Aleksey Petrov <a.petrov1024@yandex.ru>
2020-05-11 14:15:37 +0300

    Добавлена модель из консольной версии и создан прототип ViewModel

---
+++ 2048_Xamarin/2048_Xamarin/ViewModel.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Game2048_Model;
+
+
+
+namespace _2048_Xamarin
+{
+    class ViewModel: INotifyPropertyChanged
+    {
+        #region Types
+        public enum Actions { None, Left, Right, Up, Down };
+        #endregion
+
+        #region Fields
+        private Queue<string> values = new Queue<string>();
+        private Model model;
+        private int hFieldSize;
+        private int vFieldSize;
+        private int targetValue;
+        #endregion
+
+        #region Properties
+        public int TargetValue
+        {
+            get
+            {
+                return this.targetValue;
+            }
+            set
+            {
+                if ((value != this.targetValue) && (value > 0) && (value % 2 == 0))
+                    this.targetValue = value;
+                OnPropertyChanged("TargetValue");
+            }
+        }
+        public int HFieldSize
+        {
+            get
+            {
+                return this.hFieldSize;
+            }
+            set
+            {
+                if ((value > 0) && (value != this.hFieldSize)) this.hFieldSize = value;
+                OnPropertyChanged("HFieldSize");
+            }
+        }
+        public int VFieldSize
+        {
+            get
+            {
+                return this.vFieldSize;
+            }
+            set
+            {
+                if ((value > 0) && (value != this.vFieldSize)) this.vFieldSize = value;
+                OnPropertyChanged("VFieldSize");
+            }
+        }
+        public string FieldValue
+        {
+            get
+            {
+                if (values.Count > 0)
+                    return this.values.Dequeue();
+                else
+                    return "err";
+            }
+        }
+        #endregion
+
+        #region Methods
+        public ViewModel()
+        {
+            this.VFieldSize = 4;
+            this.HFieldSize = 4;
+            this.TargetValue = 2048;
+            StartNewGame();
+        }
+
+        public void StartNewGame()
+        {
+            values = new Queue<string>();
+            this.model = new Model(HFieldSize, VFieldSize);
+            UpdateValues();
+        }
+        public void UpdateValues()
+        {
+            values.Clear();
+            for (int i = 0; i < this.model.VSize; i++)
+                for (int j = 0; j < this.model.HSize; j++)
+                {
+                    int value = this.model.Get(new Coordinates(j, i));
+                    if (value > 0)
+                        this.values.Enqueue(value.ToString());
+                    else
+                        this.values.Enqueue("");
+                }
+            OnPropertyChanged("FieldValue");
+        }
+
+        public void KeyPressed(Actions action)
+        {
+            switch (action)
+            {
+                case Actions.Down:
+                    this.model.Action(Model.Directions.Down);
+                    break;
+                case Actions.Up:
+                    this.model.Action(Model.Directions.Up);
+                    break;
+                case Actions.Left:
+                    model.Action(Model.Directions.Left);
+                    break;
+                case Actions.Right:
+                    this.model.Action(Model.Directions.Right);
+                    break;
+            }
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+        #endregion
+    }
+}
